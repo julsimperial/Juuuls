1bf8226c178d432ba239ecc9b0ec2ff6cdf0d071
crimgene1 <crimgene1@gmail.com>
2021-05-02 02:05:15 +0800

    Version 1 Julius

---
+++ Reviews.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+<p>This is a paragraph.</p>
+<p>This is another paragraph.</p>
+
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form>
+
+<form>
+  <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label><br>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label><br>
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other</label>
+</form>
+
+
+
+
+</body>
+
+</html>
